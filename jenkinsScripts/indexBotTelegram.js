d984e1926cc1e86d1cab2539a5a30ea0a2f39d0e
KikeValeroSoriano <kikevalerosoriano@gmail.com>
2024-12-22 11:02:21 +0100

    Provant

--- jenkinsScripts/indexBotTelegram.js
+++ jenkinsScripts/indexBotTelegram.js
@@ -4,6 +4,8 @@ const botToken = process.env.BOT_TOKEN;
 const msg = process.argv[2];
 const bot = new TelegramBot(botToken, {polling: true});
 
+console.log(chatId + botToken + msg);
+
 bot.sendMessage(chatId, msg)
   .then(() => {
     console.log('Message sent');
