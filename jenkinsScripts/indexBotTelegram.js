a584309526b2edf681b56e462cb2ecc9b698ef94
KikeValeroSoriano <kikevalerosoriano@gmail.com>
2024-12-22 11:22:22 +0100

    probant

--- jenkinsScripts/indexBotTelegram.js
+++ jenkinsScripts/indexBotTelegram.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 // const chatId = process.env.CHAT_ID;
 // const botToken = process.env.BOT_TOKEN;
-const chatId = process.argv[3];
+const chatId = 1311083639;
 const botToken = "7763082320:AAEIry2h0ZdzaSNGHQMAOAh-pc_jHYtPWTk";
 const msg = process.argv[2];
 const bot = new TelegramBot(botToken, {polling: true});
